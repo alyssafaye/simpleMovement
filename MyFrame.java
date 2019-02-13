0c67c181079a41f52579396dc99ed11bcdd00d79
alyssafaye <fayealicando@gmail.com>
2019-02-13 22:17:33 +0800

    updated

--- MyFrame.java
+++ MyFrame.java
@@ -1,49 +1,59 @@
 import javax.swing.JFrame;
-import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-    public class MyFrame extends JFrame implements KeyListener {
-    
-     Draw drawing;
+public class MyFrame extends JFrame implements KeyListener{
+
+    Draw drawing;
 
     public MyFrame(){
         this.drawing = new Draw();
     }
 
-      public void keyPressed(KeyEvent e){
-          if(e.getKeyCode() == KeyEvent.VK_UP){
-              drawing.moveUp();
-          }
-         else if (e.getKeyCode() == KeyEvent.VK_DOWN){
-             drawing.moveDown();
-         }
-         else if (e.getKeyCode() == KeyEvent.VK_LEFT){
-             drawing.moveLeft();
-         }
-         else if (e.getKeyCode() == KeyEvent.VK_RIGHT){
-             drawing.moveRight();
-         }
-         else if(e.getKeyCode() == KeyEvent.VK_SPACE){
-             drawing.jump();
-            System.out.println("jump");
+    public void keyPressed(KeyEvent e){
+        if(e.getKeyCode() == KeyEvent.VK_UP){
+            drawing.moveUp();
         }
-     }
-
-     public void keyReleased(KeyEvent e){
-
-     }
-
-     public void keyTyped(KeyEvent e){
-
-     }
-
-     public static void main(String args[]){
-         MyFrame gameFrame = new MyFrame();
-         Draw drawing = new Draw();
-         gameFrame.setSize(600,332);
-         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         gameFrame.setVisible(true);
-         gameFrame.getContentPane().add(gameFrame.drawing);
-         gameFrame.addKeyListener(gameFrame);
-     }
+        else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            drawing.moveRight();
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            drawing.moveDown();
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            drawing.moveLeft();
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_S){
+            drawing.attack();
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_A){
+            drawing.jump();
+        }
+         else if(e.getKeyCode() == KeyEvent.VK_D){
+            drawing.crouch();
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_W){
+            drawing.smrslt();
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_SPACE){
+            drawing.spawnEnemy();
+        }
     }
+
+    public void keyReleased(KeyEvent e){
+
+    }
+
+    public void keyTyped(KeyEvent e){
+        
+    }
+
+    public static void main(String args[]){
+        MyFrame gameFrame = new MyFrame();
+        gameFrame.setSize(670,380);
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameFrame.setVisible(true);
+        gameFrame.getContentPane().add(gameFrame.drawing);
+        gameFrame.addKeyListener(gameFrame);
+    }
+}
